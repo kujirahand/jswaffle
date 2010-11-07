c92c8922c717cd2d426e0a65edf3980f09394323
kujirahand <kujirahand@c9df2fda-4992-90a0-2967-796019ae5968>
2010-11-07 09:15:17 +0000

    first commit

---
+++ trunk/jsWaffleTemplate/src/com/kujirahand/jsWaffle/SubWaffleActivity.java
@@ -0,0 +1,21 @@
+package com.kujirahand.jsWaffle;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SubWaffleActivity extends WaffleActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+        // Check url (Intent Param)
+        Intent i = getIntent();
+        String url = i.getStringExtra("url");
+        if (url != null) {
+        	this.showPage(url);
+        }
+        else {
+        	this.showPage("file:///android_asset/www/index.html");
+        }
+    }
+
+}
